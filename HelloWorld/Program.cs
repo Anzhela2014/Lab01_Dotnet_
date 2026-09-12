4dbe2ecedaf59db7ee1054da955e90361ce40558
Lika <likareznik555@gmail.com>
2026-09-12 11:09:15 +0300

    Step 6: first C# program running

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("пРИВЕТ ОТ исп-242");
+Console.WriteLine("Авторы: Резник и Каменнова");
+Console.WriteLine("год 2026");
